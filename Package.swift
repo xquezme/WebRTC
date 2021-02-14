67e904f52621a8775d814f799fce95bb0c84e9c8
Pimenov Sergey <pimenov.sergei@gmail.com>
2021-02-14 17:44:57 +0300

    Added package.swift

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "WebRTC",
+    products: [
+        .library(
+            name: "WebRTC",
+            targets: ["WebRTC"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(name: "WebRTC", path: "WebRTC/WebRTC.xcframework")
+    ]
+)
